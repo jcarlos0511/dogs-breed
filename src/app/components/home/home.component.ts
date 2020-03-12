d39be4b69bf00daab5977db091b33cd28f797d71
Chara- <escor05_11@hotmail.com>
2020-03-11 21:36:13 -0500

    adding list  breeds

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 
 @Component({
