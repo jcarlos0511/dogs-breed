1ee417cfb62c70da8835eb7b493772d62163d419
Chara- <escor05_11@hotmail.com>
2020-03-11 23:50:31 -0500

    fixed breed's dog

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { DogsService } from "src/app/services/dogs.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+  selector: "app-home",
+  templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+  breeds: any[] = [];
+  subBreeds: any[] = [];
 
-  dogs: any[] = [];
-
-  constructor( private http: HttpClient) {
-    this.http.get('https://dog.ceo/api/breeds/list').subscribe((data:any)=>{
-      console.log(data.message);
-      this.dogs = data.message;
-    });
-   }
-
-  ngOnInit() {
-
+  constructor(private http: HttpClient, private dogService: DogsService) {
+    this.seeBreed();
+    this.seeSubBreed();
   }
 
+  ngOnInit() {}
+
+  seeBreed() {
+    this.dogService.getQuery("breeds/list").subscribe((data: any) => {
+      console.log(data.message);
+      return (this.breeds = data.message);
+    });
+  }
+
+  seeSubBreed() {
+    this.dogService.getQuery("breeds/list/all").subscribe((data: any) => {
+      console.log(data.message);
+      return (this.subBreeds = data.message);
+    });
+  }
+
+  getBreed(idx:string){
+    console.log(idx);
+  }
 }
--- src/app/services/dogs.service.ts
+++ src/app/services/dogs.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DogsService {
+ 
+  constructor(private http: HttpClient) {}
+  
+  getQuery(value: string) {
+    const url = "https://dog.ceo/api/";
+    return this.http.get(url + value);
+  }
 
-  constructor( ) { }
 }
