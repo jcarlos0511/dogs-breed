450873853528432e09c85b055bca51182601ab2a
Chara- <escor05_11@hotmail.com>
2020-04-16 17:20:57 -0500

    a

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dis/spa-animals'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/spa-animals/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
